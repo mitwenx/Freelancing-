7fd2af4d5c23510cb86dabe10e829836fd8d8286
Mitwen <mitwen18@gmail.com>
2026-02-13 21:43:21 +0530

    Create page.tsx

---
+++ app/works/page.tsx
@@ -0,0 +1,9 @@
+import WorksGrid from '../../components/WorksGrid';
+
+export default function WorksPage() {
+  return (
+    <div style={{ paddingTop: '80px' }}>
+      <WorksGrid />
+    </div>
+  );
+}
